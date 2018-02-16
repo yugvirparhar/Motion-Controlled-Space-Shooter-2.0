fed83298cb08484f01dac7f0471368f404cef9d7
Meenakshi Sivapriya <meenakshisivapriya@gmail.com>
2018-02-16 16:45:27 +0530

    Modified player controller scripts
    Uses sockets to communicate with the kinect to map the motion to the space ship.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,12 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+//using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System;
 
 [System.Serializable]
 public class Boundary
@@ -9,7 +16,17 @@ public class Boundary
 
 public class PlayerController : MonoBehaviour 
 {
-	public float speed;
+    TcpClient client;
+    Stream s;
+    StreamReader sr;
+    StreamWriter sw;
+    Thread recvT;
+    public bool ready = false;
+    public bool running = true;
+    public string message;
+    string temp;
+
+    public float speed;
 	public float tilt;
 	public Boundary boundary;
 
@@ -18,10 +35,17 @@ public class PlayerController : MonoBehaviour
 	public float fireRate;
 	private float nextfire;
 
-	void Update()
+
+    void Start()
+    {
+        new Thread(initThread).Start();
+    }
+
+    void Update()
 	{
-		if (Input.GetButton ("Fire1") && Time.time > nextfire)
+		if (message=="0" && Time.time > nextfire)
 		{
+            print("shoot");
 			nextfire = Time.time + fireRate;
 			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
 			//GetComponent<audio> ().Play;
@@ -29,21 +53,101 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-	void FixedUpdate()
+    public void initThread()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 5204);
+        s = client.GetStream();
+        sr = new StreamReader(s);
+        sw = new StreamWriter(s);
+        sw.AutoFlush = true;
+
+        recvT = new Thread(recvThread);
+        recvT.Start();
+
+        ready = true;
+    }
+
+    public void send(string message)
+    {
+        temp = message;
+        new Thread(sendThread).Start();
+    }
+
+    public void sendThread()
+    {
+        try
+        {
+            string name = temp;
+            sw.WriteLine(name);
+        }
+        catch
+        {
+        }
+    }
+
+    public void recvThread()
+    {
+        print("RecvThread started..");
+        while (running)
+        {
+            try
+            {
+                message = sr.ReadLine();
+                print("Recieved : " + message);
+                //FixedUpdate(message);
+                // WindowsVoice.theVoice.speak(message);
+
+            }
+            catch (Exception e)
+            {
+                print(e);
+            }
+        }
+    }
+
+
+    void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+        if (message == "2")
+        {
+            print("left");
+           // float moveHorizontal = Input.GetAxis("Horizontal");
+            //float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
-		GetComponent<Rigidbody> ().position = new Vector3 
-			(
-			Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax),
-			0.0f,	
-			Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax)
-		);
+            Vector3 movement = new Vector3(-0.2f, 0.0f, 0.0f);
+ 
 
-		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);
-	}
+            GetComponent<Rigidbody>().velocity = movement * speed;
+            GetComponent<Rigidbody>().position = new Vector3
+                (
+                Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+
+            GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        }
+
+        if (message == "3")
+        {
+            print("right");
+            // float moveHorizontal = Input.GetAxis("Horizontal");
+            //float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(0.2f, 0.0f, 0.0f);
+
+
+            GetComponent<Rigidbody>().velocity = movement * speed;
+            GetComponent<Rigidbody>().position = new Vector3
+                (
+                Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+
+            GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        }
+    }
+
 }
 
--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -1,45 +1,132 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System;
 
-	public class PlayerController2 : MonoBehaviour 
-	{
-		public float speed;
-		public float tilt;
-	    public float xMin, xMax, zMin, zMax;
+public class PlayerController2 : MonoBehaviour
+{
+    TcpClient client;
+    Stream s;
+    StreamReader sr;
+    StreamWriter sw;
+    Thread recvT;
+    public bool ready = false;
+    public bool running = true;
+    public string message;
+    string temp;
 
-		public GameObject shot;
-		public Transform shotSpawn;
-		public float fireRate;
-		private float nextfire;
+    public float speed;
+    public float tilt;
+    public float xMin, xMax, zMin, zMax;
 
-		void Update()
-		{
-			if (Input.GetButton ("Fire1.1") && Time.time > nextfire)
-			{
-				nextfire = Time.time + fireRate;
-				Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
-				//GetComponent<audio> ().Play;
-				GetComponent<AudioSource>().Play();
-			}
-		}
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextfire;
 
-		void FixedUpdate()
-		{
-			float moveHorizontal = Input.GetAxis ("Horizontal1");
-			float moveVertical = Input.GetAxis ("Vertical1");
+    void Start()
+    {
+        new Thread(initThread).Start();
+    }
 
-			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-			GetComponent<Rigidbody>().velocity = movement * speed;
-			GetComponent<Rigidbody> ().position = new Vector3 
-				(
-					Mathf.Clamp (GetComponent<Rigidbody> ().position.x, xMin, xMax),
-					0.0f,	
-					Mathf.Clamp (GetComponent<Rigidbody> ().position.z, zMin, zMax)
-				);
+    void Update()
+    {
+        if (message=="0" && Time.time > nextfire)
+        {
+            nextfire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            //GetComponent<audio> ().Play;
+            GetComponent<AudioSource>().Play();
+        }
+    }
 
-			GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody> ().velocity.x * -tilt);
-		}
-	}
+    public void initThread()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 8887);
+        s = client.GetStream();
+        sr = new StreamReader(s);
+        sw = new StreamWriter(s);
+        sw.AutoFlush = true;
 
+        recvT = new Thread(recvThread);
+        recvT.Start();
 
+        ready = true;
+    }
 
+    public void send(string message)
+    {
+        temp = message;
+        new Thread(sendThread).Start();
+    }
+
+    public void sendThread()
+    {
+        try
+        {
+            string name = temp;
+            sw.WriteLine(name);
+        }
+        catch
+        {
+        }
+    }
+
+    public void recvThread()
+    {
+        print("RecvThread started..");
+        while (running)
+        {
+            try
+            {
+                message = sr.ReadLine();
+                print("Recieved : " + message);
+                //FixedUpdate(message);
+                // WindowsVoice.theVoice.speak(message);
+
+            }
+            catch (Exception e)
+            {
+                print(e);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // float moveHorizontal = Input.GetAxis("Horizontal1");
+        //float moveVertical = Input.GetAxis("Vertical1");
+        if (message == "2")
+        {
+            print("left 2");
+            Vector3 movement = new Vector3(-0.2f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().velocity = movement * speed;
+            GetComponent<Rigidbody>().position = new Vector3
+                (
+                    Mathf.Clamp(GetComponent<Rigidbody>().position.x, xMin, xMax),
+                    0.0f,
+                    Mathf.Clamp(GetComponent<Rigidbody>().position.z, zMin, zMax)
+                );
+
+            GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        }
+
+        if (message == "3")
+        {
+            print("right 2");
+            Vector3 movement = new Vector3(0.2f, 0.0f, 0.0f);
+            GetComponent<Rigidbody>().velocity = movement * speed;
+            GetComponent<Rigidbody>().position = new Vector3
+                (
+                    Mathf.Clamp(GetComponent<Rigidbody>().position.x, xMin, xMax),
+                    0.0f,
+                    Mathf.Clamp(GetComponent<Rigidbody>().position.z, zMin, zMax)
+                );
+
+            GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        }
+    }
+
+}
